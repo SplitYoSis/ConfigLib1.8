2fc5f3adb97cdf23f7e8e19ea450b57c6e45cea8
SplitYoSis <87956691+SplitYoSis@users.noreply.github.com>
2023-06-09 11:14:16 +0300

    Added an Unbreakable option to ItemFlags

--- src/main/java/dev/splityosis/configsystem/configsystem/logics/ItemStackConfigLogic.java
+++ src/main/java/dev/splityosis/configsystem/configsystem/logics/ItemStackConfigLogic.java
@@ -69,7 +69,8 @@ public class ItemStackConfigLogic extends ConfigTypeLogic<ItemStack> {
                 ItemFlag itemFlag = ItemFlag.valueOf(flag);
                 meta.addItemFlags(itemFlag);
             }catch (Exception e){
-                //
+                if (flag.equalsIgnoreCase("unbreakable"))
+                    meta.setUnbreakable(true);
             }
         }
 
@@ -105,6 +106,8 @@ public class ItemStackConfigLogic extends ConfigTypeLogic<ItemStack> {
             List<String> flags = new ArrayList<>();
             for (ItemFlag itemFlag : meta.getItemFlags())
                 flags.add(itemFlag.name());
+            if (meta.isUnbreakable())
+                flags.add("UNBREAKABLE");
             config.set(path + ".item-flags", flags);
         }
 
